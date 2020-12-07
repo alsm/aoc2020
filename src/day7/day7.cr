5daa8c4164fe25f563aac1d5d65b21034a22e675
alsm <al@hronrad.eu>
2020-12-07 10:07:34 +0000

    day 7

---
+++ src/day7/day7.cr
@@ -0,0 +1,42 @@
+class Day7
+  @input = Hash(String, Hash(String, Int32)).new
+  def initialize(f : String)
+    File.read_lines(f).each do |l|
+      holds, bags = l.split(" contain ")
+      holds = holds.rstrip("bags").strip
+      @input[holds] = Hash(String, Int32).new if !@input.has_key?(holds)
+      bags.split(", ").each do |b|
+        if m = /((\d) (\w+\s\w+))+/.match(b)
+          @input[holds][m[3]] = m[2].to_i
+        end
+      end
+    end
+  end
+
+  def part1
+    bags = Set(String).new
+    list = Deque.new(["shiny gold"])
+    while !list.empty?
+      bag = list.shift
+      @input.each do |k,v|
+        if v.has_key?(bag)
+          bags << k
+          list.push(k)
+        end
+      end
+    end
+    bags.size
+  end
+
+  def number_contains(b : String, n : Int32)
+    @input[b].empty? ? n : @input[b].map{|k,v| x = n * number_contains(k, v).as(Int32)}.sum + n
+  end
+
+  def part2
+    number_contains("shiny gold", 1) - 1
+  end
+end
+
+d = Day7.new("day7.txt")
+pp d.part1
+pp d.part2
