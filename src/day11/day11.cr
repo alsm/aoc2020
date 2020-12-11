aafd7926931b455832e5a2d64bde92f8af03f4ce
Al S-M <asm@localhost.localdomain>
2020-12-11 14:28:13 +0000

    neater & faster

--- src/day11/day11.cr
+++ src/day11/day11.cr
@@ -1,84 +1,110 @@
 class Day11
-  @input = Hash(Tuple(Int32,Int32), Char).new
+  @input = Array(Array(Char)).new
+
+  class Cell
+    property kind : Char
+    def initialize(@kind, @level : Int32)
+    end
+
+    def floor?
+      @kind == '.'
+    end
+
+    def occupied?
+      @kind == '#'
+    end
+
+    def next_state(neighbours : Array(Cell))
+      case @kind
+      when '#'
+        neighbours.select{|n| n.occupied?}.size >= @level ? 'L' : '#'
+      when 'L'
+        neighbours.select{|n| n.occupied?}.size > 0 ? 'L' : '#'
+      else
+        '.'
+      end
+    end
+  end
 
   class Grid
-    @floor : Hash(Tuple(Int32,Int32), Char)
+    @floor = Array(Array(Cell)).new
+    @neighbours : Proc(Int32, Int32, Array(Cell))
     @width : Int32
     @height : Int32
+    @vectors = [[0,1], [1,1], [1,0], [1,-1], [-1,0], [-1,-1], [0,-1], [-1, 1]]
 
-    def initialize(@floor : Hash(Tuple(Int32,Int32), Char), @occupied : Int32, @alt : Bool)
-      @width = @floor.keys.map{|x,y| x}.max
-      @height = @floor.keys.map{|x,y| y}.max
-    end
-  
-    def in_bounds(l : Tuple(Int32, Int32))
-      x,y = l
-      0 <= x <= @width && 0 <= y <= @height
-    end
-  
-    def is_floor(l : Tuple(Int32, Int32))
-      @floor[l] == '.'
-    end
-
-    def is_occupied(l : Tuple(Int32, Int32))
-      @floor[l] == '#'
-    end
-  
-    def neighbours(l : Tuple(Int32, Int32))
-      return neighbours2(l) if @alt
-      x,y = l
-      [{x+1, y}, {x-1, y}, {x, y-1}, {x, y+1}, {x+1, y+1}, {x-1, y-1}, {x+1, y-1}, {x-1, y+1}]
-        .select{|p| in_bounds(p) && !is_floor(p)}
-    end
-
-    def neighbours2(l : Tuple(Int32, Int32))
-      x,y = l
-      [{1, 0}, {-1, 0}, {0, -1}, {0, 1}, {1, 1}, {-1, -1}, {1, -1}, {-1, 1}]
-        .compact_map{|v| s = seat_from_vector(l, v); in_bounds(s) && !is_floor(s) ? s : nil}
-    end
-
-    def seat_from_vector(l : Tuple(Int32, Int32), v : Tuple(Int32, Int32))
-      new_l = {l[0]+v[0], l[1]+v[1]}
-      while in_bounds(new_l)
-        return new_l if !is_floor(new_l)
-        new_l = {new_l[0]+v[0], new_l[1]+v[1]}
+    def initialize(floor : Array(Array(Char)), level : Int32)
+      @width = floor[0].size
+      @height = floor.size
+      floor.each_with_index do |l,i|
+        @floor << Array(Cell).new
+        l.each do |c|
+          @floor[i] << Cell.new(c, level)
+        end
       end
-      new_l
+      if level == 4
+        @neighbours = ->neighbours1(Int32, Int32)
+      else
+        @neighbours = ->neighbours2(Int32, Int32)
+      end
+    end
+  
+    def in_bounds(x, y,)
+      0 <= x < @width && 0 <= y < @height
+    end
+  
+    def neighbours1(x, y)
+      @vectors.compact_map{|n| nx, ny = n; in_bounds(nx+x, ny+y) ? @floor[ny+y][nx+x] : nil}
     end
 
-    def transform(l : Tuple(Int32, Int32))
-      {'.' => '.',
-       'L' => neighbours(l).select{|s| is_occupied(s)}.size > 0 ? 'L' : '#',
-       '#' => neighbours(l).select{|s| is_occupied(s)}.size >= @occupied ? 'L' : '#'}[@floor[l]]
+    def neighbours2(x, y)
+      @vectors.compact_map{|v| vx, vy = v; seat_from_vector(x, y, vx, vy)}
+    end
+
+    def seat_from_vector(x, y, vx, vy)
+      x += vx
+      y += vy
+      while in_bounds(x, y)
+        return @floor[y][x] if !@floor[y][x].floor?
+        x += vx
+        y += vy
+      end
+      nil
     end
 
     def iterate
-      new_floor = Hash(Tuple(Int32,Int32), Char).new
-      @floor.keys.each{|k| new_floor[k]=transform(k).not_nil!}
-      new_floor
+      changes = Array(Tuple(Cell, Char)).new
+      @floor.each_with_index do |l,i|
+        l.each_with_index do |c,j|
+          ns = c.next_state(@neighbours.call(j,i))
+          changes << {c, ns} if ns != c.@kind
+        end
+      end
+      changes.each{|e| c,s = e; c.kind = s}
+      changes.size
     end
 
     def run
       while
-        new_floor = iterate
-        return @floor.values.select{|s| s == '#'}.size if @floor.values == new_floor.values
-        @floor = new_floor
+        changes = iterate
+        return @floor.flatten.select{|c| c.occupied?}.size if changes == 0
       end
     end
   end
 
   def initialize(f : String)
     File.read_lines(f).each_with_index do |l,i|
-      l.chars.each_with_index{|c,j| @input[{i,j}] = c}
+      @input << Array(Char).new
+      l.chars.each_with_index{|c,j| @input[i] << c}
     end
   end
 
   def part1
-    Grid.new(@input.dup, 4, false).run
+    Grid.new(@input.dup, 4).run
   end
 
   def part2
-    Grid.new(@input.dup, 5, true).run
+    Grid.new(@input.dup, 5).run
   end
 end
 
